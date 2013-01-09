650945fbff5f8fd460232f467d8c4e8966ed8940
ACY <andres29111990@gmail.com>
2013-01-09 15:49:42 -0400

    layout

--- st/application/Bootstrap.php
+++ st/application/Bootstrap.php
@@ -3,6 +3,13 @@
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
 
+  protected function _initAutoloaders() {
 
+          $test_loader = new Zend_Application_Module_Autoloader( array(   'namespace' => 'Modulo1',
+                                                                              'basePath'  => APPLICATION_PATH . '/modules/modulo1'
+          ));
+
+  }
+  
 }
 
---
+++ st/application/modules/orden/controllers/CrearController.php
@@ -0,0 +1,18 @@
+<?php
+
+class Orden_CrearController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+        // action body
+    }
+
+
+}
+

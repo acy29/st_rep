c0f271a29d75c68a9a33154c7829bf5ca246d9a0
ACY <andres29111990@gmail.com>
2013-01-23 15:59:13 -0400

    mensajes

--- st/application/Bootstrap.php
+++ st/application/Bootstrap.php
@@ -3,37 +3,57 @@
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
 
+  /*
+  *the modules
+  */
   protected function _initAutoloaders() {
 
          $test_loader = new Zend_Application_Module_Autoloader( array(   'namespace' => 'Orden','basePath'  => APPLICATION_PATH . '/modules/orden'
           ));
 
-         $test_loader = new Zend_Application_Module_Autoloader( array(   'namespace' => 'Usuario','basePath'  => APPLICATION_PATH . '/modules/usuario'
+         $test_loader = new Zend_Application_Module_Autoloader( array(   'namespace' => 'User','basePath'  => APPLICATION_PATH . '/modules/user'
           ));
 
          $test_loader = new Zend_Application_Module_Autoloader( array(   'namespace' => 'Acl','basePath'  => APPLICATION_PATH . '/modules/acl'
           ));
 
+          $test_loader = new Zend_Application_Module_Autoloader( array(   'namespace' => 'Login','basePath'  => APPLICATION_PATH . '/modules/login'
+          ));
+
+
   }
+/*
+*Cargo los js css y layout
+*/
 
   protected function _initViewHelpers() { 
       $this->bootstrap('layout');     
        
+      //***layout
       $layout = $this->getResource('layout'); 
-       $view = $layout->getView(); 
-       $view->doctype('XHTML1_STRICT'); 
-       $view->headMeta()->appendHttpEquiv('Content-Type', 'text/html;charset=utf-8'); 
-       
-       $view->headTitle('SI'); 
+      $view = $layout->getView(); 
 
+      $view->doctype('XHTML1_STRICT'); 
+      $view->headMeta()->appendHttpEquiv('Content-Type', 'text/html;charset=utf-8'); 
+       
+      $view->headTitle('SI'); 
+
+      //*** the css here
       $view->headLink(array('rel'  => 'favicon','href' => '/img/favicon.ico',), 'PREPEND')
             ->appendStylesheet('/st_rep/st/public/css/estilo.css')
             ->prependStylesheet( '/st_rep/st/public/css/ui-lightness/jquery-ui-1.9.2.custom.min.css', 'screen',true,array('id' => 'my_stylesheet'));
             
-
-    	 $view->headScript()->appendFile('/st_rep/st/public/js/jquery.tools.min.js');
-    	 $view->headScript()->appendFile('/st_rep/st/public/js/jquery.dataTables.min.js');
+       //*** the js here
+       $view->headScript()->appendFile('/st_rep/st/public/js/jquery.tools.min.js');
+       $view->headScript()->appendFile('/st_rep/st/public/js/jquery.dataTables.min.js');
        $view->headScript()->appendFile('/st_rep/st/public/js/jquery-ui-1.9.2.custom.min.js');
+
+       $view->headScript()->appendFile('/st_rep/st/public/js/mesage/jquery.noty.js');
+       $view->headScript()->appendFile('/st_rep/st/public/js/mesage/topRight.js');
+       $view->headScript()->appendFile('/st_rep/st/public/js/mesage/topCenter.js');
+       $view->headScript()->appendFile('/st_rep/st/public/js/mesage/default.js');
+       $view->headScript()->appendFile('/st_rep/st/public/js/mesage/mesage.js');
+       
 
        //***helper_cliente cliente buscar
        $view->headScript()->appendFile('/st_rep/st/public/js/clientebuscar.js');
@@ -41,25 +61,27 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
     }
   
 
-  protected function _initHelperPath(){
+ /* protected function _initHelperPath(){
 
          Zend_Controller_Action_HelperBroker::addPath(APPLICATION_PATH .'/controllers/helpers');
-      }
+      }*/
 
   protected function _initAutoload() {
 
-         require_once 'Zend/Loader/Autoloader.php';
+      //*** load the preDispacher
+      $front = Zend_Controller_Front::getInstance();
+      $front->registerPlugin(new Zend_Controller_Plugin_CheckAccess());
+    }
 
-         $loader = Zend_Loader_Autoloader::getInstance();
-         $loader->setFallbackAutoloader(true);
-         
-     }
-   
+  
   protected function _initViewResources() {
 
-         Zend_Layout::startMvc();
+     //Zend_Layout::startMvc();
      }
 
+  /*
+  *load Helper
+  */
   protected function _initResourceLoader(){
 
     $loader = $this->getResourceLoader();
